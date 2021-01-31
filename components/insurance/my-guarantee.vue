6f5d9d290832ca5f582c6bdf17bf384fbbbd211e
jihuanyu <ji2240224966@163.com>
2021-01-31 16:38:38 +0800

    'show'

--- components/insurance/my-guarantee.vue
+++ components/insurance/my-guarantee.vue
@@ -41,7 +41,7 @@
           <td>{{ fixD(item.volume, 8) }}</td>
           <td>{{ item.dueDate }}</td>
           <td>
-            <button class="b_b_button" @click="toActive(item)">
+            <button class="b_b_button" @click="toActive(item)" v-if="item.dueDate!='Expired'">
               {{ $t("Table.outSure") }}
             </button>
           </td>

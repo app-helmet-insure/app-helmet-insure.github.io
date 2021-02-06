d59ab6c6a12e51ddb40f279fcda943fff8aaafad
jihuanyu <ji2240224966@163.com>
2021-02-06 16:02:56 +0800

    'LPT'

--- components/mining/helmet_hcct_pool.vue
+++ components/mining/helmet_hcct_pool.vue
@@ -82,7 +82,7 @@
             <a
               href="https://exchange.pancakeswap.finance/?_gl=1*1dr4rcd*_ga*MTYwNTE3ODIwNC4xNjEwNjQzNjU4*_ga_334KNG3DMQ*MTYxMTgxMTMzMi42Ny4wLjE2MTE4MTEzMzIuMA..#/add/0x948d2a81086A075b3130BAc19e4c6DEe1D2E3fE8/0x17934fef9fC93128858e9945261524ab0581612e"
               target="_blank"
-              >Go to Pancake Pool</a
+              >Get LPT</a
             >
           </section>
         </div>

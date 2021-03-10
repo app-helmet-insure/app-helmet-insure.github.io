c99d0cab1b328f63a9fb9522ece679bb8cdaf555
jihuanyu <ji2240224966@163.com>
2021-03-10 19:31:48 +0800

    'update'

--- components/flashmining/bnb500_pool.vue
+++ components/flashmining/bnb500_pool.vue
@@ -8,7 +8,11 @@
       alt=""
       v-if="expired"
     />
-    <img class="circle left" src="~/assets/img/helmet/leftCircle.png" alt="" />
+    <img
+      class="circle right"
+      src="~/assets/img/helmet/rightCircle.png"
+      alt=""
+    />
     <div class="text">
       <div class="coin">
         <h3>

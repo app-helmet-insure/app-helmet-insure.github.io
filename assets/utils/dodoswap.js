4e812b108c354f4a0132fc193fd12234059ce95c
“jihuanyu” <ji2240224966@163.com>
2021-04-29 17:05:56 +0800

    line

---
+++ assets/utils/dodoswap.js
@@ -0,0 +1,12 @@
+import Axios from 'axios';
+
+export const dodoswap = (data) => {
+    let data = {
+        fromTokenAddress: '', // sellAdress
+        fromTokenDecimals: '', // sellDecimals
+        toTokenAddress: '', // buyAdress
+        toTokenDecimals: '', // buyDecimals
+        fromTokenAddress: '', // sellAdress
+        fromTokenAddress: '', // sellAdress 
+    };
+};

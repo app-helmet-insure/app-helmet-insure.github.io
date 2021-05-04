3bf65fc0ecd67a84249d50c100637da3f478d6ff
“jihuanyu” <ji2240224966@163.com>
2021-05-04 23:19:57 +0800

    'number'

--- components/common/slider.vue
+++ components/common/slider.vue
@@ -101,7 +101,7 @@
               <use xlink:href="#icon-flashmining"></use>
             </svg>
             {{ $t("Header.FlashMining") }}
-            <i class="num">2</i>
+            <i class="num">1</i>
           </nuxt-link>
         </div>
         <div class="menu_item">

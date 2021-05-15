3c0520b00e0d5abe18b4f8ef3cfae11f01016907
xubowenshizi <xubowenshizi@gmail.com>
2021-05-15 10:40:12 +0800

    locale

--- components/nft/nft_title.vue
+++ components/nft/nft_title.vue
@@ -73,7 +73,7 @@
       </button>
     </div>
     <div class="card_button" v-else>
-      <button class="one" @click="handleClickReward">领取</button>
+      <button class="one" @click="handleClickReward">{{ $t("NFT.getReward") }}</button>
     </div>
     <div class="card_tips">
       <img src="~/assets/img/nft/home_tip.png" alt="" />

2469ef9a6acd707e8f6e0ccad1c71dd0c0983fbb
“jihuanyu” <ji2240224966@163.com>
2021-05-27 19:27:04 +0800

    'Swap'

--- interface/swap.js
+++ interface/swap.js
@@ -5,6 +5,7 @@ import { toWei, fromWei } from '~/assets/utils/web3-fun.js';
 import bus from '~/assets/js/bus';
 import Message from '~/components/common/Message';
 import { web3 } from '~/assets/utils/web3-obj.js';
+
 export const SwapHelmet = async (amount, token1, token2, callback) => {
     let TOKEN1 = new Token(
         token1.chainId,
@@ -36,120 +37,127 @@ export const SwapHelmet = async (amount, token1, token2, callback) => {
     );
     let RouterAdress = '0x10ED43C718714eb63d5aA57B78B54704E256024E';
     try {
-        const PairAdress = Pair.getAddress(TOKEN1, TOKEN2);
-        const PairContracts = await PairContract(PairAdress);
-        const SwapContracts = await SwapContract(RouterAdress);
-        let TokenArray = await PairContracts.methods
-            .token0()
-            .call()
-            .then((res) => {
-                return res;
-            });
-        let Reserves =
-            TokenArray.toLowerCase() ==
-            '0x948d2a81086a075b3130bac19e4c6dee1d2e3fe8'
-                ? true
-                : false;
-        await PairContracts.methods
-            .getReserves()
-            .call()
-            .then((res) => {
-                if (res) {
-                    SwapContracts.methods
-                        .getAmountOut(
-                            token1.decimals == 18
-                                ? toWei(amount)
-                                : BigNumber(
-                                      (
-                                          amount * Math.pow(10, token1.decimals)
-                                      ).toString()
-                                  ).toFixed(),
-                            Reserves ? res.reserve1 : res.reserve0,
-                            Reserves ? res.reserve0 : res.reserve1
-                        )
-                        .call()
-                        .then((result) => {
-                            callback({
-                                amount: BigNumber(
+        if (
+            token1.address.toLowerCase() ==
+            '0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c'
+        ) {
+            const PairAdress = Pair.getAddress(TOKEN1, TOKEN2);
+            const PairContracts = await PairContract(PairAdress);
+            const SwapContracts = await SwapContract(RouterAdress);
+            let TokenArray = await PairContracts.methods
+                .token0()
+                .call()
+                .then((res) => {
+                    return res;
+                });
+            let Reserves =
+                TokenArray.toLowerCase() ==
+                '0x948d2a81086a075b3130bac19e4c6dee1d2e3fe8'
+                    ? true
+                    : false;
+            await PairContracts.methods
+                .getReserves()
+                .call()
+                .then((res) => {
+                    if (res) {
+                        SwapContracts.methods
+                            .getAmountOut(
+                                token1.decimals == 18
+                                    ? toWei(amount)
+                                    : BigNumber(
+                                          (
+                                              amount *
+                                              Math.pow(10, token1.decimals)
+                                          ).toString()
+                                      ).toFixed(),
+                                Reserves ? res.reserve1 : res.reserve0,
+                                Reserves ? res.reserve0 : res.reserve1
+                            )
+                            .call()
+                            .then((result) => {
+                                callback({
+                                    amount: BigNumber(
+                                        (
+                                            result /
+                                            Math.pow(10, token2.decimals)
+                                        ).toString()
+                                    ).toFixed(),
+                                    router: false,
+                                });
+                            });
+                    } else {
+                        callback(0);
+                    }
+                });
+        } else {
+            const PairAdress = Pair.getAddress(TOKEN1, BNBTOKEN);
+            const PairContracts = await PairContract(PairAdress);
+            const SwapContracts = await SwapContract(RouterAdress);
+            let TokenArray = await PairContracts.methods
+                .token0()
+                .call()
+                .then((res) => {
+                    return res;
+                });
+            let Reserves =
+                TokenArray.toLowerCase() ==
+                '0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c'
+                    ? true
+                    : false;
+            await PairContracts.methods
+                .getReserves()
+                .call()
+                .then((res) => {
+                    if (res) {
+                        SwapContracts.methods
+                            .getAmountOut(
+                                BigNumber(
                                     (
-                                        result / Math.pow(10, token2.decimals)
+                                        amount * Math.pow(10, token1.decimals)
                                     ).toString()
                                 ).toFixed(),
-                                router: false,
+                                Reserves ? res.reserve1 : res.reserve0,
+                                Reserves ? res.reserve0 : res.reserve1
+                            )
+                            .call()
+                            .then((res1) => {
+                                let amount = BigNumber(
+                                    (
+                                        res1 / Math.pow(10, BNBTOKEN.decimals)
+                                    ).toString()
+                                ).toFixed();
+                                SwapHelmet(
+                                    amount,
+                                    {
+                                        address:
+                                            '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
+                                        chainId: 56,
+                                        decimals: 18,
+                                        name: 'WBNB Token',
+                                        symbol: 'WBNB',
+                                    },
+                                    {
+                                        address:
+                                            '0x948d2a81086A075b3130BAc19e4c6DEe1D2E3fE8',
+                                        chainId: 56,
+                                        decimals: 18,
+                                        name: 'Helmet.insure',
+                                        symbol: 'Helmet',
+                                    },
+                                    (result) => {
+                                        callback({
+                                            amount: result.amount,
+                                            router: true,
+                                        });
+                                    }
+                                );
                             });
-                        });
-                } else {
-                    callback(0);
-                }
-            });
-    } catch (error) {
-        const PairAdress = Pair.getAddress(TOKEN1, BNBTOKEN);
-        const PairContracts = await PairContract(PairAdress);
-        const SwapContracts = await SwapContract(RouterAdress);
-        let TokenArray = await PairContracts.methods
-            .token0()
-            .call()
-            .then((res) => {
-                return res;
-            });
-        let Reserves =
-            TokenArray.toLowerCase() ==
-            '0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c'
-                ? true
-                : false;
-        await PairContracts.methods
-            .getReserves()
-            .call()
-            .then((res) => {
-                if (res) {
-                    SwapContracts.methods
-                        .getAmountOut(
-                            BigNumber(
-                                (
-                                    amount * Math.pow(10, token1.decimals)
-                                ).toString()
-                            ).toFixed(),
-                            Reserves ? res.reserve1 : res.reserve0,
-                            Reserves ? res.reserve0 : res.reserve1
-                        )
-                        .call()
-                        .then((res1) => {
-                            let amount = BigNumber(
-                                (
-                                    res1 / Math.pow(10, BNBTOKEN.decimals)
-                                ).toString()
-                            ).toFixed();
-                            SwapHelmet(
-                                amount,
-                                {
-                                    address:
-                                        '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
-                                    chainId: 56,
-                                    decimals: 18,
-                                    name: 'WBNB Token',
-                                    symbol: 'WBNB',
-                                },
-                                {
-                                    address:
-                                        '0x948d2a81086A075b3130BAc19e4c6DEe1D2E3fE8',
-                                    chainId: 56,
-                                    decimals: 18,
-                                    name: 'Helmet.insure',
-                                    symbol: 'Helmet',
-                                },
-                                (result) => {
-                                    callback({
-                                        amount: result.amount,
-                                        router: true,
-                                    });
-                                }
-                            );
-                        });
-                } else {
-                    callback(0);
-                }
-            });
-    }
+                    } else {
+                        callback(0);
+                    }
+                });
+        }
+    } catch (error) {}
 };
 export const SwapBNBforTokens = async (
     { activeData, SwapNumber, MinReward, SwapRouter },

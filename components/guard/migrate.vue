222a75bc2a2f6683c7cff7b51f76efebd4644c32
“jihuanyu” <ji2240224966@163.com>
2021-07-01 16:25:48 +0800

    fix

--- components/guard/migrate.vue
+++ components/guard/migrate.vue
@@ -1,5 +1,14 @@
 <template>
-  <div class="guard_migrate"></div>
+  <div class="guard_migrate">
+    <div class="guard_migrate_wrap">
+      <h3 class="guard_migrate_title">Migrate to Polygon</h3>
+      <div class="input_box">
+        <input type="text" name="" id="" placeholder="输入划转数量" />
+        <span class="max">最大量</span>
+        <p>HELMET</p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -10,7 +19,60 @@ export default {};
 .guard_migrate {
   width: 100%;
   min-width: 1200px;
-  background: #fcc;
-  height: 100px;
+  height: 420px;
+  background: #ffffff;
+  box-shadow: 0px 4px 8px 0px rgba(155, 155, 155, 0.02);
+  border-radius: 5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  &_wrap {
+    width: 320px;
+    h3 {
+      font-size: 24px;
+      font-family: IBMPlexSans-SemiBold, IBMPlexSans;
+      font-weight: 600;
+      color: #17173a;
+      line-height: 32px;
+      text-align: center;
+    }
+    .input_box {
+      margin-top: 30px;
+      width: 100%;
+      height: 40px;
+      border: 2px solid #e8e8e8;
+      border-radius: 5px;
+      display: flex;
+      align-items: center;
+      position: relative;
+      input {
+        width: 250px;
+        height: 100%;
+        padding-left: 12px;
+        &::placeholder {
+          color: rgba(23, 23, 58, 0.4);
+        }
+      }
+      > span {
+        position: absolute;
+        display: block;
+        font-size: 12px;
+        padding: 6px 8px;
+        height: 24px;
+        background: #f8f9fa;
+        border-radius: 5px;
+        border: 1px solid #e8e8eb;
+        color: #17173a;
+        right: 81px;
+        display: flex;
+        align-items: center;
+      }
+      > p {
+        height: 100%;
+        flex: 1;
+        border-left: 1px solid #e8e8e8;
+      }
+    }
+  }
 }
 </style>

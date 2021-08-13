8231dc867095780a4b6e54b2201d0603ebce5fe3
sunxuan <1406243464@qq.com>
2021-08-14 00:31:29 +0800

    add ibo

---
+++ plugins/element-ui.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+// 导入UI
+import {
+  Slider
+} from 'element-ui'
+Vue.use(Slider)
+
+export default ({ store }) => {
+  // 由于通过全局参数传入,所以 this.$notify 不能通过 this.$notify.success 调用,只可以使用 this.$notify({type: 'success'}) 方式调用
+  Vue.prototype.$notify = (options) => {
+    Object.assign(options, {
+      offset: 50,
+      customClass: store.state.dictionary.isToolBarShow ? 'barShow' : '',
+    })
+    return Notification(options)
+  }
+}

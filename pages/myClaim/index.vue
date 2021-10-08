dacbe18e8be67dee03d12efd4ce599d7a24d9544
HelmetPink <ji2240224966@163.com>
2021-10-08 16:25:55 +0800

    delete lang

--- pages/myClaim/index.vue
+++ pages/myClaim/index.vue
@@ -1,4 +1,26 @@
+<template>
+  <div class="main_container">
+    <MyClaim></MyClaim>
+  </div>
+</template>
+
 <script>
-import MyClaim from "~/pages/_lang/myClaim/index.vue";
-export default MyClaim;
+import MyClaim from "~/components/insurance/my-claim.vue";
+export default {
+  layout: "home",
+  name: "myClaim",
+  components: {
+    MyClaim,
+  },
+};
 </script>
+
+<style lang='scss' scoped>
+@media screen and (min-width: 750px) {
+  .main_container {
+    width: 80%;
+    min-width: 1026px;
+    margin: 20px auto 0;
+  }
+}
+</style>

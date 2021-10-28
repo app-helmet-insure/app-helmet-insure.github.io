51c4ad1fc923e14ea51a16ec464b2e6e22c2de77
HelmetPink <ji2240224966@163.com>
2021-10-29 00:02:05 +0800

    BNB1000 airdrop

--- config/airdrop.js
+++ config/airdrop.js
@@ -9,7 +9,7 @@ export const AirdropList = {
       Symbol: "BNB1000",
       Decimals: 18,
       TokenAddress: "0x224b33139a377a62d4BaD3D58cEDb7807AE228eB",
-      PoolAddress: "0x0472b48623aAFCcbE440CC178B1D0ab7c5263daF",
+      PoolAddress: "0xE4574280b431580794713F50f68C6ccb9134AD8a",
       Rewards: 0,
       Balance: 0,
     },
